2fa3463c48357472b4576e8257e59d27f7808194
Luca Ordronneau <luca.ordronneau@eisti.fr>
2021-12-05 17:23:26 +0100

    Remove method

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 import uvicorn
 from fastapi import FastAPI
 import pickle
-from routers import predict
+from routers import predict, model
 from src.run import run
 
 # Create the app object
@@ -18,22 +18,6 @@ async def index():
     """
     return {'message': 'Hello, stranger'}
 
-# Route with a single parameter
-@app.get('/{name}')
-async def get_name(name: str):
-    """[summary]
-
-    Args:
-        name (str): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return {'message': f'Hello, {name}'}
-
 app.include_router(predict.router)
- 
-# Run the API with uvicorn
-# if __name__ == '__main__':
-    # uvicorn.run(app, host="127.0.0.1", port=8000)
-    # run("/home/eisti/Documents/ING3-IA/Architecture Microservice/euromillions/app/src/data/EuroMillions_numbers.csv")
+
+app.include_router(model.router)
