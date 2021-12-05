30fb0c3a0b9de3c174f5040012ef88519005320d
Luca Ordronneau <luca.ordronneau@eisti.fr>
2021-12-05 18:02:05 +0100

    Get technical infos of the model (f1 score, precision, name)

--- app/routers/model.py
+++ app/routers/model.py
@@ -7,6 +7,27 @@ sys.path.append('/../classes/Draw.py')
 from classes.Draw import Draw, DrawPredict
 from joblib import load
 import numpy as np
+from src.model import getF1score, getPrecision
 
 
 router = APIRouter()
+
+@router.get('/model')
+async def getInfos():
+    
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parentdir = os.path.dirname(currentdir)
+    model = load(parentdir + '/src/data/model/model.jolib')
+
+    with open(parentdir + '/src/data/test/test.npy', 'rb') as f:
+        X_test = np.load(f)
+        y_test = np.load(f)
+        
+    y_pred = model.predict(X_test)
+    
+    return {
+        "F1 Score" : round(getF1score(y_test, y_pred)*100,2),
+        "Accuracy Score" : round(getPrecision(y_test, y_pred)*100,2),
+        "Model Name": type(model).__name__,
+        "Parameters" : "A compléter"
+    }
