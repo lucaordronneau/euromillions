6564e808117543648b2ee3a7fde9df05e57c6cc0
Luca Ordronneau <luca.ordronneau@eisti.fr>
2021-12-09 15:38:04 +0100

    Add retrain function

--- app/routers/model.py
+++ app/routers/model.py
@@ -7,7 +7,8 @@ sys.path.append('/../classes/Draw.py')
 from classes.Draw import Draw, DrawPredict
 from joblib import load
 import numpy as np
-from src.model import getF1score, getPrecision
+from src.model import getF1score, getPrecision, formatData, splitData, model, saveModel
+from src.dataset import createDraws
 import csv      
 
 router = APIRouter()
@@ -47,3 +48,26 @@ async def addDraw(draw: Draw):
         writer = csv.writer(write_obj, delimiter=';')
         draw=[lines,draw.Date, draw.N1, draw.N2,draw.N3,draw.N4,draw.N5,draw.E1,draw.E2,draw.Gain]
         writer.writerow(draw)
+        
+@router.post('/model/retrain')
+async def retrain():
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parentdir = os.path.dirname(currentdir)
+    csv_path = parentdir + '/src/data/EuroMillions_numbers.csv'
+    
+    df = pd.read_csv(csv_path, sep=";")
+    draws = df[["N1", "N2", "N3", "N4", "N5", "E1", "E2"]].to_numpy()
+    draws = createDraws(draws)
+    X, y = formatData(draws)
+    X_train, X_test, y_train, y_test = splitData(X, y)
+    clf = model(X_train, y_train)
+    saveModel(clf)
+    
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parentdir = os.path.dirname(currentdir)
+    path = parentdir + '/src/data/test/test.npy'
+    
+    with open(path, 'wb') as f:
+        np.save(f, np.array(X_test))
+        np.save(f, np.array(y_test))
+    
